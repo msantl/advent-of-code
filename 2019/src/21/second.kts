9f02e0903f303692ab7b6e15ae0cbb33e5f2e03c
Matija Santl <matijas@noom.com>
2019-12-21 11:16:58 +0100

    Day twentyone solutions

---
+++ 2019/src/21/second.kts
@@ -0,0 +1,188 @@
+import java.io.File
+import java.util.stream.Collectors
+import kotlin.math.max
+
+fun getValueModes(command: Int): List<Int> {
+    var cmd: String = command.toString()
+    while (cmd.length < 5) cmd = "0" + cmd
+
+    return listOf(
+        cmd.substring(3, 5).toInt(),
+        cmd.substring(2, 3).toInt(),
+        cmd.substring(1, 2).toInt(),
+        cmd.substring(0, 1).toInt()
+    )
+}
+
+var memory: MutableMap<Int, Int> = mutableMapOf()
+var ip: Int = 0
+
+var relativeBase: Int = 0
+var halt: Boolean = false
+
+fun runOnce(input: List<Int>, returnOnOutput: Boolean): MutableList<Int> {
+    var inputPointer: Int = 0
+    val results: MutableList<Int> = mutableListOf()
+
+    while (!halt) {
+        val command: Int = memory.getOrDefault(ip++, 0)
+        val modes: List<Int> = getValueModes(command)
+
+        when (modes.get(0)) {
+            1 -> {
+                var val1: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(1) == 0) val1 = memory.getOrDefault(val1, 0)
+                else if (modes.get(1) == 2) val1 = memory.getOrDefault(val1 + relativeBase, 0)
+
+                var val2: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(2) == 0) val2 = memory.getOrDefault(val2, 0)
+                else if (modes.get(2) == 2) val2 = memory.getOrDefault(val2 + relativeBase, 0)
+
+                var addr: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(3) == 2) addr = addr + relativeBase
+
+                memory.put(addr, val1 + val2)
+            }
+            2 -> {
+                var val1: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(1) == 0) val1 = memory.getOrDefault(val1, 0)
+                else if (modes.get(1) == 2) val1 = memory.getOrDefault(val1 + relativeBase, 0)
+
+                var val2: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(2) == 0) val2 = memory.getOrDefault(val2, 0)
+                else if (modes.get(2) == 2) val2 = memory.getOrDefault(val2 + relativeBase, 0)
+
+                var addr: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(3) == 2) addr = addr + relativeBase
+
+                memory.put(addr, val1 * val2)
+            }
+            3 -> {
+                var addr: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(1) == 2) addr = addr + relativeBase
+
+                // Input
+                memory.put(addr, input.get(inputPointer))
+                inputPointer++
+            }
+            4 -> {
+                var res: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(1) == 0) res = memory.getOrDefault(res, 0)
+                else if (modes.get(1) == 2) res = memory.getOrDefault(res + relativeBase, 0)
+
+                // Output
+                if (returnOnOutput) {
+                    return mutableListOf(res)
+                }
+                results.add(res)
+            }
+            5 -> {
+                var val1: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(1) == 0) val1 = memory.getOrDefault(val1, 0)
+                else if (modes.get(1) == 2) val1 = memory.getOrDefault(val1 + relativeBase, 0)
+
+                var val2: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(2) == 0) val2 = memory.getOrDefault(val2, 0)
+                else if (modes.get(2) == 2) val2 = memory.getOrDefault(val2 + relativeBase, 0)
+
+                if (val1 != 0) ip = val2
+            }
+            6 -> {
+                var val1: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(1) == 0) val1 = memory.getOrDefault(val1, 0)
+                else if (modes.get(1) == 2) val1 = memory.getOrDefault(val1 + relativeBase, 0)
+
+                var val2: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(2) == 0) val2 = memory.getOrDefault(val2, 0)
+                else if (modes.get(2) == 2) val2 = memory.getOrDefault(val2 + relativeBase, 0)
+
+                if (val1 == 0) ip = val2
+            }
+            7 -> {
+                var val1: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(1) == 0) val1 = memory.getOrDefault(val1, 0)
+                else if (modes.get(1) == 2) val1 = memory.getOrDefault(val1 + relativeBase, 0)
+
+                var val2: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(2) == 0) val2 = memory.getOrDefault(val2, 0)
+                else if (modes.get(2) == 2) val2 = memory.getOrDefault(val2 + relativeBase, 0)
+
+                var addr: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(3) == 2) addr = addr + relativeBase
+
+                memory.put(addr, if (val1 < val2) 1 else 0)
+            }
+            8 -> {
+                var val1: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(1) == 0) val1 = memory.getOrDefault(val1, 0)
+                else if (modes.get(1) == 2) val1 = memory.getOrDefault(val1 + relativeBase, 0)
+
+                var val2: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(2) == 0) val2 = memory.getOrDefault(val2, 0)
+                else if (modes.get(2) == 2) val2 = memory.getOrDefault(val2 + relativeBase, 0)
+
+                var addr: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(3) == 2) addr = addr + relativeBase
+
+                memory.put(addr, if (val1 == val2) 1 else 0)
+            }
+            9 -> {
+                var value: Int = memory.getOrDefault(ip++, 0)
+                if (modes.get(1) == 0) value = memory.getOrDefault(value, 0)
+                else if (modes.get(1) == 2) value = memory.getOrDefault(value + relativeBase, 0)
+
+                relativeBase += value
+            }
+            99 -> halt = true
+        }
+    }
+    return results
+}
+
+val map: MutableMap<Pair<Int, Int>, Int> = mutableMapOf()
+
+fun main() {
+    val commands: List<Int> = File("second.in")
+        .readText()
+        .trim()
+        .split(",")
+        .map { it.toInt() }
+        .toList()
+
+    halt = false
+    relativeBase = 0
+    ip = 0
+    commands.withIndex().forEach { it -> memory.put(it.index, it.value) }
+    val inputList = listOf(
+        "OR B J",
+        "AND C J",
+        "NOT J J",
+        "AND D J",
+        "OR E T",
+        "OR H T",
+        "AND T J",
+        "NOT A T",
+        "OR T J",
+        "RUN"
+    )
+    var input: MutableList<Int> = mutableListOf()
+
+    for (cmd in inputList) {
+        for (c in cmd) {
+            input.add(c.toInt())
+        }
+        input.add(10)
+    }
+
+    val res = runOnce(input, false)
+
+    for (r in res) {
+        if (r > 0 && r < 256) {
+            print(r.toChar())
+        } else {
+            println(r)
+        }
+    }
+}
+
+main()
